ef00ca7ad65dd3d965cb310a59b6d34c80538072
xiaodou <927105917@qq.com>
2017-03-30 20:40:31 +0800

    temple of python on Mar/30/2017

---
+++ 000-temple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# ================================================================
+#
+# Describe: This is temple of python
+# Author: XiaoDou
+# Update time: Mar/30/2017
+# Development environment: CentOS 6.5/python 2.7.12
+#
+# ================================================================
+
+print "----------------------------------------------------------"
+print "\n"
