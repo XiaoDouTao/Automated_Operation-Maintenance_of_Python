a23018a5d85dcd9ceda5f06b656412828c64ce83
XiaoDou <zhongtao_90@live.com>
2017-04-08 09:53:47 +0800

    Update 000-temple.py

--- 000-temple.py
+++ 000-temple.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
+#coding:utf-8
 
-# ================================================================
-#
-# Describe: This is temple of python
-# Author: XiaoDou
-# Update time: Mar/30/2017
-# Development environment: CentOS 6.5/python 2.7.12
-#
-# ================================================================
+'''
+================================================================
+
+ Describe: This is temple of python
+ Author: XiaoDou
+ Update time: Mar/30/2017
+ Development environment: CentOS 6.5/python 2.7.12
+
+================================================================
+'''
 
 print "----------------------------------------------------------"
 print "\n"
